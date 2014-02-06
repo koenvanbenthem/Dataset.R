7304997e8588b9f8fb3f219be2f6c51036cc803e
Koen van Benthem <koenvanbenthem@gmail.com>
2014-02-06 20:10:52 +0100

    Included survival

--- data.R
+++ data.R
@@ -8,13 +8,13 @@
 #######################
 
 #######################
-# Main object "Lepricorn": an individual of our species of interest, the Irish lepricorn, small in size, but with great powers. To make it more French for Timothee, we assume that their favourite food is camembert.
+# Main object "Leprechaun": an individual of our species of interest, the Irish Leprechaun, small in size, but with great powers. To make it more French for Timothee, we assume that their favourite food is camembert.
 #
 # The object contains the following values
 
 # Static [these numbers do not change after initialisation]
 #  V ID (unique identifier number)																	[integer]
-#  V pID (two numbers referring to the parents of the lepricorn, if none: NA)						[vector of two integers]
+#  V pID (two numbers referring to the parents of the Leprechaun, if none: NA)						[vector of two integers]
 #  V Year of birth (timestep at which the individual was born)										[integer]
 #  - Genome (?) (two vectors of length N coding for both chromosomes of N loci in the genome.)		[two vectors of N integers]
 
@@ -44,27 +44,28 @@ YR<-0
 ####################################################
 
 setClass(
-	Class="Lepricorn",
+	Class="Leprechaun",
 	representation=representation(
 		ID = "integer",
 		pID = "integer",
 		age = "integer",
 		Birth = "integer",
 		alive = "logical",
-		size = "numeric"
+		size = "numeric",
+		sex = "character"
 	)
 
 )
 
 ############### Definition of the basic methods (for printing to the screen and initialisation)
 ###############################################################################################
-setMethod("show","Lepricorn",
+setMethod("show","Leprechaun",
 	function(object){
-		cat(object@ID,"\t",object@size,"\t",object@age,"\t(",object@pID[1],",",object@pID[2],")\t",object@Birth,"\t",object@alive,"\n",sep="")
+		cat(object@ID,"\t",object@size,"\t",object@age,"\t",object@sex,"\t(",object@pID[1],",",object@pID[2],")\t",object@Birth,"\t",object@alive,"\n",sep="")
 	}
 )
 
-setMethod("initialize","Lepricorn",function(.Object,parent1,parent2){
+setMethod("initialize","Leprechaun",function(.Object,parent1,parent2){
 	if(missing(parent1)){parent1<-NA; weight1<-5+2*runif(1)}else{weight1<-pop[[parent1]]@size}
 	if(missing(parent2)){parent2<-NA; weight2<-5+2*runif(1)}else{weight2<-pop[[parent2]]@size}
 	.Object@age<-as.integer(0)
@@ -73,13 +74,71 @@ setMethod("initialize","Lepricorn",function(.Object,parent1,parent2){
 	.Object@Birth<-as.integer(YR)
 	.Object@alive<-TRUE
 	.Object@size<-0.5*weight1+0.5*weight2
+
+	if(runif(1)>0.5){.Object@sex<-'F'}else{.Object@sex<-'M'}
+
 	CID<<-as.integer(CID+1)
 	
 	return(.Object)
 })
 
+################### Definition of more biologically relevant methods (e.g. survival)
+####################################################################################
+
+# Implementing the famous bathtub, ages 1 to 20
+bathtub<-function(age){
+	p<-0.6*exp(-age/4)+(-1+exp(age*log(2)/20))
+	p[p>1]<-1
+	return(p)
+}
+
+# Applying the bathtub in a surival function
+setGeneric("Surv",function(Object){standardGeneric("Surv")})
+
+setMethod("Surv","Leprechaun",function(Object){
+	
+	if(runif(1)>bathtub(Object@age)){
+		Object@alive<-FALSE
+		ALIVE<<-ALIVE[ALIVE!=Object@ID]
+	}
+	
+	return(Object)
+	
+})
+
+# Simple function, simply adds 1 to the age
+setGeneric("Age",function(Object){standardGeneric("Age")})
+
+setMethod("Age","Leprechaun",function(Object){
+	Object@age<-as.integer(Object@age+1)
+	return(Object)
+})
+
+
 ############### Creating an initial population with 10 individuals
-pop<-c(new("Lepricorn"))
-for(i in 2:10){
-	pop<-c(pop,new("Lepricorn"))
+pop<-c(new("Leprechaun"))
+for(i in 2:100){
+	pop<-c(pop,new("Leprechaun"))
+}
+
+############### List of living individuals [their indices], this will save time later, because dead individuals are not looped over
+ALIVE<-1:length(pop)
+
+############### The start of time
+for(YR in 1:10){
+	cat("\nAt the beginning of year:",YR,"\nThere are:",length(ALIVE),"Leprechauns\n-----------------\n")
+	
+	#### Survival
+	for(i in ALIVE){
+		pop[[i]]<-Surv(pop[[i]])
+		pop[[i]]<-Age(pop[[i]])
+		cat(i,"\t")
+	}
+	
+	
+	#### Growth
+	
+	
+	#### Reproduction
+		
 }
