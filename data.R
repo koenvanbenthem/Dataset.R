7c5af1496c4a36e14da32e4a357b476b9a5bb4ec
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2014-02-11 17:23:04 +0100

    some considerations about genetic architecture

--- data.R
+++ data.R
@@ -17,10 +17,16 @@
 #  V ID (unique identifier number) [integer]
 #  V pID (two numbers referring to the parents of the Leprechaun, if none: NA) [vector of two integers]
 #  V Year of birth (timestep at which the individual was born)	[integer]
-#  - Genome (?) (two vectors of length N coding for both chromosomes of N loci in the genome.) [two vectors of N integers]
-#  - Sex (M/F)
+#  (- Genome (?) (two vectors of length N coding for both chromosomes of N loci in the genome.) [two vectors of N integers]
 #  - Heritable phenotypic trait value of interest (z) (e.g. birth weight) (changes/constant through life depending on trait)
-#  - (Possibly: breeding value A)
+#  - (Possibly: breeding value A))
+#  - Rather simulate physically independent loci, otherwise we need to simulate recombination on the chromosomes. Over short time periods, 100 recombinations fragments (ie independent loci) sounds realistic.
+#  - Explicit coding of traits by many independent diploid loci. The simplest model: z = mean + sum_loci(a1_locus + a2_locus) + environment. One can add explicit dominance and epistasis, as well as interactions with environment. 
+#  - A possibility is to draw the a of the different alleles from a N(0,V). Each locus can have a different V, and thus a different importance. 
+#  - A large number of loci (>20) will give easily patterns expected from quantitative genetics. We can draw randomly the number of loci per trait. 
+#  - My main concern at the moment is the initialisation of the genetic diversity: it will be hard to avoid a fast decline of diversity at the beginning.
+#  - We probably do not need mutations.
+#  - Sex (M/F). Could be genetically determined by on locus, thus allowing random fluctuations of sex ratio and thus population structure.
 
 # Dynamic [these numbers do change after initialisation]
 #  V alive (boolean, true/false) [boolean]
@@ -37,6 +43,7 @@
 # - Number of offspring: n_offspring(a,x,z,d) (poisson distribution)
 # - Offspring size x distribution: x_offspring(x_mother,z_mother,x_father,z_father) (not required if working with stages) (prability density function)
 # - Offspring trait z distribution: z_offspring(x_mother,z_mother,x_father,z_father,A_mother,A_father) + rnorm(0,V_E) (prability density function)
+# - For survival and reproduction, we can consider functions of the form W=exp(-((z-Optimum)^2)/(2*width)), that is, gaussian stabilizing selection. We could then let the Optimum shift. 
 
 ####################
 ####### Environmental aspects
