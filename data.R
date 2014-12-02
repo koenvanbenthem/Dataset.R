6ff7eaae2d855450cf29de92c596495b2c13f182
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2014-12-02 11:30:41 +0100

    Wrap the code in a function + cleaning

--- data.R
+++ data.R
@@ -1,15 +1,15 @@
 ##### In this file we will try to come up with some things ####
-###############################################################
+###############################################################%
 
-########################
+########################%
 ####### Type of program
 ## Written in R
 ## Object Oriented
-#######################
+#######################%
 
-#######################
+#######################%
 # Main object "Leprechaun": an individual of our species of interest, the Irish Leprechaun, small in size, but with great powers. To make it more French for Timothee, we assume that their favourite food is camembert. Leprechaun is not very choosy, and mates completely random.
-##########################################
+##########################################%
 
 # The object contains the following values
 
@@ -34,7 +34,7 @@
 #  - stage (possibly instead of/in addition to age) (juvenile, adult, etc)
 #  V size (x)
 
-####################
+####################%
 ###### Relations that need to be defined between size (x), age (a), trait (z) and vital rates
 # (- Possibly include population density d in functions)
 # V Survival(a,x,z,d) (logistic function) 
@@ -73,51 +73,40 @@
 # Offspring added to population
 ### End of timestep
 
-####################################################
+####################################################%
 ################ GLOBAL VARIABLES AND COUNTERS #####
-####################################################
-
-
-############### Selection parameters ###############
+####################################################%
 folder<-"Simple"
 converter<-paste("Data/",folder,"/conv.csv",sep="")
 dir.create(file.path("Data", folder))
 cat("filename\tSS1\tSS2\tFS1\tFS2\tCAMSEL\tSDCAM",file=converter,append=FALSE)
 
+################ Random seed #######################
+#set.seed(12)
 
 
-# for(SurvivalSelection1 in C(0.1)){
-# for(SurvivalSelection2 in c(0)){
-# for(fertilitySelection1 in c(0.1)){
-# for(fertilitySelection2 in c(0)){
-# for(camembertSelection in c(0)){
-# for(SDCamembert in c(1000)){
-  
-SurvivalSelection1<-0.1 #linear coefficient on a logit scale for Survival ~ ... + size +size^2
-SurvivalSelection2<-(0.0) #quadratic coefficient on a logit scale for Survival ~ ... + size + size^2; negative value=balancing selection
+LeprechaunSimul<-function(SurvivalSelection1=0.1,SurvivalSelection2=0,fertilitySelection1=0.1,fertilitySelection2=0,
+                          camembertSelection=0.1,survivalPenaltyForRepro=0,MeanCamembert=5000,SDCamembert=1000,
+                          MeanBirthSize=10,lowBoundGrowth=0.99,highBoundGrowth=1.5,SexualMaturity=4,
+                          PlasticityBirthSize=1,PlasticityHunting=0,MaternalEffect=0.1,MeanRepro=10){
 
-fertilitySelection1<-0.1 #linear coefficient on a log scale for reproduction ~ ... + size + size^2
-fertilitySelection2<-(0.0) #quadratic coefficient on a log scale for reproduction ~ ... + size + size^2; negative value=balancing selection
-
-camembertSelection<-0.1 #increases the number of offspring, multiplied by Number Of Camemberts at the power 1/3 (because its a volume, uhuh)
-survivalPenaltyForRepro<-0
-
-SDZ<-1
-SDH<-1
-####################################################
-
+#SurvivalSelection1<-0.1 #linear coefficient on a logit scale for Survival ~ ... + size +size^2
+#SurvivalSelection2<-(0.0) #quadratic coefficient on a logit scale for Survival ~ ... + size + size^2; negative value=balancing selection
+#fertilitySelection1<-0.1 #linear coefficient on a log scale for reproduction ~ ... + size + size^2
+#fertilitySelection2<-(0.0) #quadratic coefficient on a log scale for reproduction ~ ... + size + size^2; negative value=balancing selection
+#camembertSelection<-0.1 #increases the number of offspring, multiplied by Number Of Camemberts at the power 1/3 (because its a volume, uhuh)
+#survivalPenaltyForRepro<-0
 ################ Environmental parameters ##########
-MeanCamembert<-5000
-SDCamembert<-1000
-
-lowBoundGrowth<-0.99 # minimal growth rate
-highBoundGrowth<-1.5 # maximal growth rate
-
-PlasticityBirthSize<-1
-PlasticityHunting<-0
-MaternalEffect<-0.1
-
-SexualMaturity<-4 #the 50% reproductive size 
+#MeanCamembert<-5000
+#SDCamembert<-1000
+#lowBoundGrowth<-0.99 # minimal growth rate
+#highBoundGrowth<-1.5 # maximal growth rate
+#PlasticityBirthSize<-1
+#PlasticityHunting<-0
+#MaternalEffect<-0.1
+#SexualMaturity<-4 #the 50% reproductive size 
+#MeanBirthSize<-10
+#MeanRepro<-10
 
 filename<-paste("Data/",folder,"/pop_SS1_",SurvivalSelection1*10,"_SS2_",SurvivalSelection2*100,"_FS1_",fertilitySelection1*10,
                 "_FS2_",fertilitySelection2*100,"_CAMSEL_",camembertSelection*10,"_SDCAM_",SDCamembert,".csv",sep="")
@@ -125,79 +114,56 @@ cat("\n",filename,"\t",SurvivalSelection1,"\t",SurvivalSelection2,"\t",fertility
 
 
 
-################ Random seed #######################
-set.seed(12)
-
-################ Counter for the IDs ###############
+################ Counter for the IDs ###############%
 CID<-as.integer(1) 
-
-
-################ Counter for the current year ######
+################ Counter for the current year ######%
 YR<-0 
-
 ################ Base population parameters ########
 
-MeanBirthSize<-10
-# needed to make survival ~ size relative on age
-meanGrowth<-prod(runif(10000,lowBoundGrowth,highBoundGrowth))^(1/10000)
-MeanRepro<-10
-
-############### Genetic determinism Z ################
+############### Genetic determinisms  ##############%
 dominance<-1 # for additive effects only, must be 0
 overdominance<-0 # non-null values generate overdominance
 
 nbLoci<-10 #number of loci controling the trait phenotype
 nbAlleles<-10 #number of existing alleles per loci
 
-gvaluesZ<-array(data=NA,dim=c(nbAlleles,nbAlleles,nbLoci),dimnames=list(paste("A",1: nbAlleles,sep=""),paste("A",1: nbAlleles,sep=""),paste("L",1:nbLoci,sep=""))) # Initialising a matrix that will contain the genotypic effects on the/a trait
+SDZ<-1#standard deviation of locus effect on size
+SDH<-1#standard deviation of locus effect on hunting
 
-for(L in 1:nbLoci)
-{
-  # Setting the effects for the homozygotes [all loci]
-  effect<-abs(rnorm(n=1,mean=0,sd=SDZ))# alter the locus importance in a realistic way (many small-effect loci, few major loci)
-  diag(gvaluesZ[,,L])<-2*rnorm(n=dim(gvaluesZ)[1],mean=0,sd=effect)
-  # Setting the effects for the heterozygotes
-  for(A in 1:(nbAlleles-1))# loop for off-diagonal = heterozygotes (additive and dominance effects)
-  {
-    for (D in (A+1):nbAlleles)
+  FunctionGvalues<-function(nbLoci=10,nbAlleles=10,dominance=0.5,overdominance=0,SD=1)
     {
-      d<-dominance*runif(n=1,min=-0.5-overdominance,max=0.5+overdominance)
-      gvaluesZ[A,D,L]<-(0.5-d)*gvaluesZ[A,A,L]+(0.5+d)*gvaluesZ[D,D,L] # mean of additive effects + dominance, over diagonal
-      gvaluesZ[D,A,L]<-(0.5-d)*gvaluesZ[A,A,L]+(0.5+d)*gvaluesZ[D,D,L] # the same below diagonal    
+      gvalues<-array(data=NA,dim=c(nbAlleles,nbAlleles,nbLoci),dimnames=list(paste("A",1: nbAlleles,sep=""),paste("A",1: nbAlleles,sep=""),paste("L",1:nbLoci,sep=""))) # Initialising a matrix that will contain the genotypic effects on the/a trait
+      for(L in 1:nbLoci)
+        {
+        # Setting the effects for the homozygotes [all loci]
+          effect<-abs(rnorm(n=1,mean=0,sd=SD))# alter the locus importance in a realistic way (many small-effect loci, few major loci)
+          diag(gvalues[,,L])<-2*rnorm(n=dim(gvalues)[1],mean=0,sd=effect)
+          # Setting the effects for the heterozygotes
+          for(A in 1:(nbAlleles-1))# loop for off-diagonal = heterozygotes (additive and dominance effects)
+          {
+            for (D in (A+1):nbAlleles)
+            {
+              d<-dominance*runif(n=1,min=-0.5-overdominance,max=0.5+overdominance)
+              gvalues[A,D,L]<-(0.5-d)*gvalues[A,A,L]+(0.5+d)*gvalues[D,D,L] # mean of additive effects + dominance, over diagonal
+              gvalues[D,A,L]<-(0.5-d)*gvalues[A,A,L]+(0.5+d)*gvalues[D,D,L] # the same below diagonal    
+            }
+          }
+        }
+    return(gvalues)
     }
-  }
-}
-############### Genetic determinism H ################
+############### Genetic determinism Z
+gvaluesZ<-FunctionGvalues(nbLoci = 10,nbAlleles = 10,dominance = 0.1,overdominance = 0,SD=SDZ)
+############### Genetic determinism H 
+gvaluesH<-FunctionGvalues(nbLoci = 10,nbAlleles = 10,dominance = 0.5,overdominance = 0,SD=SDH)
 
-gvaluesH<-array(data=NA,dim=c(nbAlleles,nbAlleles,nbLoci),dimnames=list(paste("A",1: nbAlleles,sep=""),paste("A",1: nbAlleles,sep=""),paste("L",1:nbLoci,sep=""))) # Initialising a matrix that will contain the genotypic effects on the/a trait
-
-for(L in 1:nbLoci)
-{
-  # Setting the effects for the homozygotes [all loci]
-  effect<-abs(rnorm(n=1,mean=0,sd=SDH))# alter the locus importance in a realistic way (many small-effect loci, few major loci)
-  diag(gvaluesH[,,L])<-2*rnorm(n=dim(gvaluesH)[1],mean=0,sd=effect)
-  # Setting the effects for the heterozygotes
-  for(A in 1:(nbAlleles-1))# loop for off-diagonal = heterozygotes (additive and dominance effects)
-  {
-    for (D in (A+1):nbAlleles)
-    {
-      d<-dominance*runif(n=1,min=-0.5-overdominance,max=0.5+overdominance)
-      gvaluesH[A,D,L]<-(0.5-d)*gvaluesH[A,A,L]+(0.5+d)*gvaluesH[D,D,L] # mean of additive effects + dominance, over diagonal
-      gvaluesH[D,A,L]<-(0.5-d)*gvaluesH[A,A,L]+(0.5+d)*gvaluesH[D,D,L] # the same below diagonal    
-    }
-  }
-}
-
-
-
-#vector of expected sizes for all ages:
+#######vector of expected sizes for all ages#####
 age<-1:20
 highBoundGrowthAge<-(highBoundGrowth+age-1)/age 
 Growth<-c(1,sapply(X = highBoundGrowthAge,FUN = function(x){mean(c(lowBoundGrowth,x))}))#with a 1 for the non-growth of the first year
 
-####################################################
+####################################################%
 ############### Definition of the class ############
-####################################################
+####################################################%
 
 setClass(
 	Class="Leprechaun",
@@ -220,8 +186,8 @@ setClass(
 
 )
 
-############### Definition of the basic methods (for printing to the screen and initialisation)
-###############################################################################################
+############### Definition of the basic methods (for printing to the screen and initialisation)######
+###############################################################################################%
 setMethod("show","Leprechaun",
 	function(object){
 		cat(object@ID,"\t",object@size,"\t",object@camemberts,"\t",object@age,"\t",object@sex,"\t(",object@pID[1],",",object@pID[2],")\t",object@Birth,"\t",object@alive,"\n",sep="")
@@ -286,10 +252,10 @@ setMethod("initialize","Leprechaun",function(.Object,parent1,parent2){#parent1 i
 	return(.Object)
 })
 
-################### Definition of more biologically relevant methods (e.g. survival)
-####################################################################################
+################### Definition of more biologically relevant methods (e.g. survival)######
+####################################################################################%
 
-# Implementing the famous bathtub, ages 1 to 20
+##### Implementing the famous bathtub, ages 1 to 20#####
 bathtub<-function(age){
   p<-0.4*exp(-age/4)+(-1+exp(age*log(2)/20))
   p[p>1]<-1
@@ -300,13 +266,13 @@ bathtub<-function(age){
 # Incorporate the effect of size to the survival function
 sizeSurvival<-function(age,size,camemberts){
   
-  sizedeviation<-size-(MeanBirthSize*prod(Growth[1:(age+1)]))
+  #sizedeviation<-size-(MeanBirthSize*prod(Growth[1:(age+1)]))
 
   p<-bathtub(age)
   if(p<1)#because size does not prevent animals of maximal age to die out
     {
       plogit<-log(p/(1-p))
-      Philogit<-plogit-SurvivalSelection1*sizedeviation-SurvivalSelection2*sizedeviation^2
+      Philogit<-plogit-SurvivalSelection1*(size-15)-SurvivalSelection2*(size-15)^2
       p<-exp(Philogit)/(1+exp(Philogit))
       if(camemberts<100)
         {
@@ -379,7 +345,7 @@ setMethod("Sex","Leprechaun",function(Object){
 setGeneric("Num_off",function(Object){standardGeneric("Num_off")})
 
 setMethod("Num_off","Leprechaun",function(Object){  
-  lambda<-exp(log(MeanRepro)+fertilitySelection1*Object@size/10+fertilitySelection2*(Object@size/10)^2
+  lambda<-exp(log(MeanRepro)+fertilitySelection1*(Object@size-MeanBirthSize)/10+fertilitySelection2*((Object@size-MeanBirthSize)/10)^2
                 +camembertSelection*((Object@camemberts)^(1/3)-survivalPenaltyForRepro)/10)
   repro<-rpois(n=1,lambda=lambda)
   
@@ -399,12 +365,12 @@ setMethod("Food","Leprechaun",function(Object,Camams){
 })
 
 
-############### Creating an initial population with 10 individuals
+############### Creating an initial population with 100 individuals#######
 pop<-c(new("Leprechaun"))
 for(i in 2:100){
 	pop<-c(pop,new("Leprechaun"))
 }
-############### Function for printing values
+############### Function for printing values########
 print_info<-function(YR,ALIVE,CID,camembert){
 	cat("\nAt the beginning of year:",YR,"\nThere are:",length(ALIVE),"Leprechauns\n-----------------\n")
 	cat("ALIVE:",ALIVE,"\n")
@@ -495,3 +461,5 @@ for(YR in 1:30){
     cat("\n",YR,"\t",pop[[i]]@ID,"\t",pop[[i]]@size,"\t",pop[[i]]@bvs,"\t",pop[[i]]@hunting,"\t",pop[[i]]@bvh,"\t",pop[[i]]@camemberts,"\t",pop[[i]]@sex,"\t",pop[[i]]@ARS,"\t",pop[[i]]@age,"\t",pop[[i]]@pID[1],"\t",pop[[i]]@pID[2],"\t",0,file=filename,append=TRUE)
   }
 }
+
+}#end of LeprechaunSimul
