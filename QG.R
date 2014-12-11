2e5bea92128cda8385338e89e812c6dc5544ba7a
timotheenivalis <timothee.bonnet@ieu.uzh.ch>
2014-12-11 13:09:33 +0100

    CI around QG estimates

--- QG.R
+++ QG.R
@@ -1,10 +1,11 @@
 ####### Load data and packages
 require(MCMCglmm)
+setwd(dir="~/GitHub/Dataset.R/Data/Simple")
+#filestem <- '/home/marjob/Documents/Popdynamics/Data/'
+#dat <- read.csv(paste(filestem,'TheDataSetV0.csv',sep=''),sep='\t')
+dat<-read.table("TheDataSetV0.csv",header=T)
+max(dat$age)
 
-filestem <- '/home/marjob/Documents/Popdynamics/Data/'
-
-
-dat <- read.csv(paste(filestem,'TheDataSetV0.csv',sep=''),sep='\t')
 dat$p1 <- as.numeric(as.character(dat$p1))
 dat$p2 <- as.numeric(as.character(dat$p2))
 dat$animal <- as.factor(dat$ID)
@@ -44,6 +45,7 @@ datPedi$mother <- as.factor(datPedi$mother)
 datPedi$father <- as.factor(datPedi$father)
 datPedi$animal <- as.factor(datPedi$animal)
 
+dat$age2<-(dat$age^2-mean(dat$age^2))/sd(dat$age^2) #standardize squared age
 
 #############################
 ## MCMC glmm Settings #######
@@ -176,11 +178,11 @@ lines(1:50,posterior.mode(model$VCV[,2])* (1:50 /meanAgeRepr) + mean(dat$z[dat$t
 ###########trying to retrieve the breeding values##############
 ###############################################################%
 ###############################################################%
-setwd(dir="C:/Users/Timothée/Documents/GitHub/Dataset.R/Data/simple")
+setwd(dir="~/GitHub/Dataset.R/Data/Simple")
 dat<-read.table(file="TheDataSetV0.csv",header=T)
 dat$p1 <- as.numeric(as.character(dat$p1))
 dat$p2 <- as.numeric(as.character(dat$p2))
-dat$animal <- as.factor(dat$ID)
+dat$animal <- dat$ID
 
 # Create two new datasets (phenotypic data and pedigree data)
 # Birth weight of individuals
@@ -198,6 +200,8 @@ datInd <- data.frame(
 aa <- tapply(dat$ARS,dat$ID,sum)
 tmp <- data.frame(animal=names(aa),R=as.numeric(aa))
 datInd <- merge(datInd,tmp,by.x='animal')
+dat <- merge(dat,tmp,by.x='animal')
+
 datInd$animal <- as.factor(datInd$animal)
 
 meantR<-tapply(datInd$R,INDEX = datInd$t,mean)
@@ -206,6 +210,12 @@ for (time in 1:48)
 {
   datInd$Rrelative[which(datInd$t==time)]<-datInd$R[which(datInd$t==time)]/meantR[time]
 }
+
+dat$Rrelative<-dat$R/(mean(dat$R))
+
+
+dat$age2<-(dat$age^2-mean(dat$age^2))/sd(dat$age^2) #standardize squared age
+
 datPedi <- data.frame(
   animal=unique(dat$ID),
   mother=as.vector(tapply(dat$p1,dat$ID,function(x) x[1])),
@@ -279,8 +289,8 @@ DR<-DZ-DM-DB
 #### Now, we should try on Z instead of Birth Weight#####
 ########################################################
 library(lme4)
-summary(lmer(z~1+t+age+I(age^2)+(1|ID),data=dat[which(dat$t>10),],REML=F))
-dat$ID
+
+summary(lmer(z~1+t+age+age2+(1|ID),data=dat[which(dat$t>10),],REML=F))
 summary(glm(z~1+t+age+age2,data=dat))
 
 priorBV <- list(
@@ -288,7 +298,7 @@ priorBV <- list(
     G1=list(V=matrix(varP/4),n=1),G2=list(V=matrix(varP/4),n=1),G3=list(V=matrix(varP/4),n=1)),
   R=list(V=matrix(varP/4),n=1))
 
-dat$age2<-(dat$age^2-mean(dat$age^2))/sd(dat$age^2)
+
 modelBV<-MCMCglmm(z~1+t+age+age2,random=~animal+ID+p1,pedigree=datPedi,data=dat[which(dat$t>10),],prior=priorBV,
                   nitt=130000,thin=100,burnin=30000,verbose=TRUE,pr=TRUE)
 summary(modelBV)
@@ -300,6 +310,8 @@ plot(mm[order(mm$m),"m"])
 BV<-modelBV$Sol[,grep(pattern = "animal*",x = colnames(modelBV$Sol))]
 MV<-modelBV$Sol[,grep(pattern = "p1.*",x = colnames(modelBV$Sol))]
 IV<-modelBV$Sol[,grep(pattern = "ID.*",x = colnames(modelBV$Sol))]
+str(modelBV$Sol)
+residuals(modelBV)
 
 animalID<-substr(x = colnames(BV),start = 8,stop=nchar(colnames(BV)))
 motherID<-substr(x = colnames(MV),start = 4,stop=nchar(colnames(MV)))
@@ -354,4 +366,94 @@ DM
 DB 
 DI
 
+#Cleaner: now accounting for uncertainty
+meanT<-tapply(mpmBV$t,mpmBV$ID,mean)
+lmBV<-as.mcmc(apply(BV,MARGIN = 1,function(x){coef(lm(x~1+meanT))[2]}))
+plot(lmBV)
+posterior.mode(lmBV*39);HPDinterval(lmBV*39)# estimated slope and CI
+
+meanTID<-tapply(mpmBV$t,mpmBV$ID,mean)
+
+meanTID<-meanTID[paste("ID.",names(meanTID),sep="")%in%colnames(IV)]
+mean(paste("ID.",names(meanTID),sep="")==colnames(IV))
+lmIV<-as.mcmc(apply(IV,MARGIN = 1,function(x){coef(lm(x~1+meanTID))[2]}))
+plot(lmIV)
+posterior.mode(lmIV*39);HPDinterval(lmIV*39)# estimated slope and CI
+
+meanTmother<-tapply(mpmBV$t,mpmBV$p1,mean)
+meanTmother<-meanTmother[paste("p1.",names(meanTmother),sep="")%in%colnames(MV)]
+mean(paste("p1.",names(meanTmother),sep="")==colnames(MV))
+lmMV<-as.mcmc(apply(MV,MARGIN = 1,function(x){coef(lm(x~1+meanTmother))[2]}))
+plot(lmMV)
+posterior.mode(lmMV*39);HPDinterval(lmMV*39)# estimated slope and CI
+
+lmRV<-DZ-lmMV*39-lmIV*39-lmBV*39
+plot(lmRV)
+posterior.mode(lmRV)
+
+posteriorDistriOfChange<-list(lmBV*39,lmIV*39,lmMV*39,lmRV)
+names(posteriorDistriOfChange)<-c("BreedingValues","Identity","Mother","Residual")
+posteriorModeOfChange<-list(posterior.mode(lmBV)*39,posterior.mode(lmIV)*39,posterior.mode(lmMV)*39,posterior.mode(lmRV))
+names(posteriorModeOfChange)<-c("BreedingValues","Identity","Mother","Residual")
+posteriorHPDOfChange<-list(HPDinterval(lmBV)*39,HPDinterval(lmIV)*39,HPDinterval(lmMV)*39,HPDinterval(lmRV))
+names(posteriorHPDOfChange)<-c("BreedingValues","Identity","Mother","Residual")
+
+ResultsQG<-list(posteriorModeOfChange,posteriorHPDOfChange,posteriorDistriOfChange)
+names(ResultsQG)<-c("mode","CI","CompleteDistri")
+save(ResultsQG,file = "ResultsQG.RData")
+
+ResultsQG$mode
 save.image(file = "AM.RData")
+
+##### Bivariate#####
+library(lme4)
+summary(lmer(Rrelative~1+age+age2+(1|t)+(1|ID),data=dat))
+summary(lm(Rrelative~1+t+age+age2,data=dat))
+
+b <- 1e+10
+B1_V <- diag(5)*b; B1_mu <- rep(0,5)
+B1_V[2,2]<-0.0001
+B1_mu[2]<-1
+
+priorMV <- list(
+  G=list(
+    G1=list(V=diag(2),n=2),
+    G2=list(V=diag(2),n=2),
+    G3=list(V=diag(1),n=1)),
+  R=list(V=matrix(data=c(1,0,0,0.00001),nrow = 2),n=2,fix=2),
+  B=list(V=B1_V, mu=B1_mu)
+)
+#priorBivariate <- list(G = list(G1 = list(V = diag(2), nu = 2),G2 = list(V = diag(2), nu = 2)), R = list(V = diag(2),nu = 2))
+
+modelMV<-MCMCglmm(cbind(z,Rrelative)~trait-1 + at.level(trait,c(1)):(age+age2+t),
+                random=~us(trait):animal + us(trait):ID + us(at.level(trait,c(1))):p1,
+                rcov=~idh(trait):units,
+                pedigree=datPedi,data=dat[which(dat$t>10),],prior=priorMV,
+                nitt=150000,thin=100,burnin=50000,verbose=TRUE,family=c('gaussian','gaussian'),pr=TRUE)
+
+save(modelMV,file='AMMV.RData')
+summary(modelMV)
+
+meanrBV<-tapply(dat$bvs,dat$t,mean)
+plot( meanrBV,xlim=c(-10,60),ylim=c(-2,2))
+abline(a = meanrBV[10]-0.02297*10,b=0.02297,lwd=3)
+abline(a = meanrBV[10]-0.02297*10,b=-0.1136)
+abline(a = meanrBV[10]-0.02297*10,b=0.1772)
+
+
+priorFitness <- list(  G=list(
+  G1=list(V=0.1,n=10),
+  G2=list(V=7,n=0.2)),
+  R=list(V=0.00001,n=0.002,fix=1),
+  B=list(mu=1,V=0.0001))
+Starting<-list(G=list(0.1,4.5),R=list(0.00001))
+
+modelFitness<-MCMCglmm(Rrelative~1,random=~animal+ID,pedigree=datPedi,
+                  data=dat[which(dat$t>10),],prior=priorFitness,start=Starting,
+                  nitt=13000,thin=10,burnin=0,verbose=TRUE,pr=TRUE)
+summary(modelFitness)
+plot(modelFitness)
+
+var(dat$R)
+summary(lmer(Rrelative~1+age+(1|ID),data=dat))
+summary(lmer(R~1+(1|ID),data=dat))
